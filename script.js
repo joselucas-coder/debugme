0d691c98e16facab6c9918d0ac6bc529e7fa0d38
Mari1434 <marianalibaniobarbosa@gmail.com>
2025-03-03 17:58:55 -0300

    v3

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+const perfil = document.querySelector('.perfil');
+const dropdown = document.getElementById('dropdown');
+
+perfil.addEventListener('click', function(event) {
+  event.stopPropagation(); // Impede que o clique se propague para o documento
+  dropdown.classList.toggle('show');
+});
+
+// Fecha o dropdown se o usuário clicar fora dele
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('.login')) {
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
+    }
+  }
+});
